9a62b85f9565c74865f275c05997dac9220eda5a
Stefan Hagspiel <shagspiel@dachcom.ch>
2019-07-06 10:04:32 +0200

    remove resource id builder

--- src/DsWebCrawlerBundle/Normalizer/ResourceNormalizer.php
+++ src/DsWebCrawlerBundle/Normalizer/ResourceNormalizer.php
@@ -7,7 +7,6 @@ use DynamicSearchBundle\Exception\RuntimeException;
 use DynamicSearchBundle\Manager\DataManagerInterface;
 use DynamicSearchBundle\Manager\TransformerManagerInterface;
 use DynamicSearchBundle\Normalizer\Resource\NormalizedDataResource;
-use DynamicSearchBundle\Normalizer\ResourceIdBuilderInterface;
 use DynamicSearchBundle\Normalizer\ResourceNormalizerInterface;
 use DynamicSearchBundle\Provider\DataProviderInterface;
 use DynamicSearchBundle\Transformer\Container\DocumentContainerInterface;
@@ -15,6 +14,7 @@ use Pimcore\Model\Asset;
 use Pimcore\Model\DataObject;
 use Pimcore\Model\Document\Page;
 use Pimcore\Model\Element\ElementInterface;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use VDB\Spider\Resource as SpiderResource;
 
@@ -24,11 +24,6 @@ class ResourceNormalizer implements ResourceNormalizerInterface
      * @var array
      */
     protected $options;
-
-    /**
-     * @var ResourceIdBuilderInterface
-     */
-    protected $resourceIdBuilder;
 
     /**
      * @var TransformerManagerInterface
@@ -72,26 +67,10 @@ class ResourceNormalizer implements ResourceNormalizerInterface
     /**
      * {@inheritDoc}
      */
-    public function setIdBuilder(ResourceIdBuilderInterface $resourceIdBuilder)
-    {
-        $this->resourceIdBuilder = $resourceIdBuilder;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getIdBuilder()
-    {
-        return $this->resourceIdBuilder;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     public function normalizeToResourceStack(ContextDataInterface $contextData, DocumentContainerInterface $documentContainer): array
     {
         if ($documentContainer->getResource() instanceof SpiderResource) {
-            $resourceId = $this->resourceIdBuilder->build($documentContainer);
+            $resourceId = $this->generateResourceId($documentContainer);
             return [new NormalizedDataResource($documentContainer, $resourceId)];
         } elseif ($contextData->getContextDispatchType() === ContextDataInterface::CONTEXT_DISPATCH_TYPE_DELETE) {
             return $this->onDeletion($documentContainer);
@@ -129,14 +108,14 @@ class ResourceNormalizer implements ResourceNormalizerInterface
                 }
             }
 
-            $resourceId = $this->resourceIdBuilder->build($documentContainer, $buildOptions);
+            $resourceId = $this->generateResourceId($documentContainer, $buildOptions);
 
             return [new NormalizedDataResource(null, $resourceId)];
         }
 
         if ($resource instanceof Asset) {
 
-            $resourceId = $this->resourceIdBuilder->build($documentContainer);
+            $resourceId = $this->generateResourceId($documentContainer);
 
             return [new NormalizedDataResource(null, $resourceId)];
         }
@@ -146,11 +125,11 @@ class ResourceNormalizer implements ResourceNormalizerInterface
             $normalizedResources = [];
             if ($this->options['locale_aware_resources'] === true) {
                 foreach (\Pimcore\Tool::getValidLanguages() as $language) {
-                    $resourceId = $this->resourceIdBuilder->build($documentContainer, ['locale' => $language]);
+                    $resourceId = $this->generateResourceId($documentContainer, ['locale' => $language]);
                     $normalizedResources[] = new NormalizedDataResource(null, $resourceId);
                 }
             } else {
-                $resourceId = $this->resourceIdBuilder->build($documentContainer);
+                $resourceId = $this->generateResourceId($documentContainer);
                 $normalizedResources[] = new NormalizedDataResource(null, $resourceId);
             }
 
@@ -213,4 +192,135 @@ class ResourceNormalizer implements ResourceNormalizerInterface
         $dataProvider->execute($newContext);
     }
 
+    /**
+     * @param DocumentContainerInterface $documentContainer
+     * @param array                      $buildOptions
+     *
+     * @return string|null
+     */
+    protected function generateResourceId(DocumentContainerInterface $documentContainer, array $buildOptions = [])
+    {
+        if ($documentContainer->getResource() instanceof SpiderResource) {
+            if ($documentContainer->hasAttribute('html')) {
+                return $this->generateResourceIdFromHtmlResource($documentContainer->getResource());
+            } elseif ($documentContainer->hasAttribute('pdf_content')) {
+                return $this->generateResourceIdFromPdfResource($documentContainer->getAttributes());
+            } else {
+                return null;
+            }
+        } elseif ($documentContainer->getResource() instanceof ElementInterface) {
+            return $this->generateResourceIdFromPimcoreResource($documentContainer->getResource(), $buildOptions);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param SpiderResource $resource
+     *
+     * @return string|null
+     */
+    protected function generateResourceIdFromHtmlResource(SpiderResource $resource)
+    {
+        /** @var Crawler $crawler */
+        $crawler = $resource->getCrawler();
+        $stream = $resource->getResponse()->getBody();
+        $stream->rewind();
+
+        $value = null;
+        $documentType = null;
+
+        $objectQuery = '//meta[@name="dynamic-search:object-id"]';
+        $pageQuery = '//meta[@name="dynamic-search:page-id"]';
+
+        if ($crawler->filterXpath($objectQuery)->count() > 0) {
+            $documentType = 'object';
+            $value = (string) $crawler->filterXpath($objectQuery)->attr('content');
+        } elseif ($crawler->filterXpath($pageQuery)->count() > 0) {
+            $documentType = 'page';
+            $value = (string) $crawler->filterXpath($pageQuery)->attr('content');
+        }
+
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($this->options['locale_aware_resources'] === true) {
+            $contentLanguage = $resource->getResponse()->getHeaderLine('Content-Language');
+            $contentLanguage = strtolower(str_replace('-', '_', $contentLanguage));
+
+            if (empty($contentLanguage)) {
+                return null;
+            }
+
+            return sprintf('%s_%s_%d', $documentType, $contentLanguage, $value);
+        }
+
+        return sprintf('%s_%d', $documentType, $value);
+
+    }
+
+    /**
+     * @param array $resourceAttributes
+     *
+     * @return string|null
+     */
+    public function generateResourceIdFromPdfResource(array $resourceAttributes)
+    {
+        $assetMeta = $resourceAttributes['asset_meta'];
+
+        if (empty($assetMeta)) {
+            return null;
+        }
+
+        $value = null;
+        if (!empty($assetMeta['id'])) {
+            $value = $assetMeta['id'];
+        }
+
+        if ($value === null) {
+            return null;
+        }
+
+        $value = sprintf('asset_%d', $value);
+
+        return $value;
+
+    }
+
+    /**
+     * @param ElementInterface $resource
+     * @param array            $buildOptions
+     *
+     * @return string|null
+     */
+    protected function generateResourceIdFromPimcoreResource(ElementInterface $resource, array $buildOptions)
+    {
+        $locale = isset($buildOptions['locale']) ? $buildOptions['locale'] : null;
+        $documentType = null;
+        $id = null;
+
+        if ($resource instanceof DataObject) {
+            $documentType = 'object';
+            $id = $resource->getId();
+        } elseif ($resource instanceof Page) {
+            $documentType = 'page';
+            $id = $resource->getId();
+        } elseif ($resource instanceof Asset\Document) {
+            $documentType = 'asset';
+            $id = $resource->getId();
+            $locale = null;
+        }
+
+        if ($documentType === null) {
+            return null;
+        }
+
+        if ($locale !== null) {
+            return sprintf('%s_%s_%d', $documentType, $locale, $id);
+        }
+
+        return sprintf('%s_%d', $documentType, $id);
+
+    }
 }
